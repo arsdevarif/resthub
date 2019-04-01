dfeacae93331b41d87f2b58fce8c80cf3e3205a4
arsdevarif <zaccxx@gmail.com>
2019-04-01 09:55:56 +0700

    update01042019

--- index.js
+++ index.js
@@ -14,22 +14,26 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 // Connect to Mongoose and set connection variable
-//mongoose.connect('mongodb+srv://arsdev:<Wellcom3>@arsdevdb-hpvyx.mongodb.net/test?retryWrites=latihan');
+//mongoose.connect('mongodb://localhost/resthub');
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://arsdev:<password>@arsdevdb-hpvyx.mongodb.net/latihan?retryWrites=true";
+const uri = "mongodb+srv://arsdev:<dYvEPlKacyHqERVr>@arsdevdb-hpvyx.mongodb.net/arsdevdb?retryWrites=true";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
-  const collection = client.db("latihan").collection("devices");
+  const collection = client.db("arsdevdb").collection("latihan");
   // perform actions on the collection object
+  
   client.close();
+
+ console.log(collection);
 });
 
-var MongoClient = mongoose.connection;
+
+//var db = mongoose.connection;
 // Setup server port
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 // Send message for default URL
-app.get('/', (req, res) => res.send('Hello World with Express'));
+app.get('/', (req, res) => res.send('<h1>some html</1>'));
 // Use Api routes in the App
 app.use('/api', apiRoutes)
 // Launch app to listen to specified port
